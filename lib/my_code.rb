e258d04cb2dfaf43efcc1ecac4fc3a72565e40b6
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-30 19:42:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,3 +10,15 @@ def map_to_negativize(array)
   
   mapped
 end
+
+def map_to_negativize(array)
+  index = 0
+  mapped = []
+  
+  while index < array.length do
+    mapped << array[index] * -1
+    index = index + 1
+  end
+  
+  mapped
+end
