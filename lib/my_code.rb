ebc4042b9fcb231b79d95e9d68e1b53173fd65c4
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-30 19:44:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,3 +22,15 @@ def map_to_no_change(array)
   
   mapped
 end
+
+def map_to_no_change(array)
+  index = 0
+  mapped = []
+  
+  while index < array.length do
+    mapped << array[index]
+    index = index + 1
+  end
+  
+  mapped
+end
